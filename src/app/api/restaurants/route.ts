8d7a303fbea4a81c6a05a6e65f41d1ebb09a1d1f
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2025-03-10 17:07:00 -0300

    Update route.ts

--- src/app/api/restaurants/route.ts
+++ src/app/api/restaurants/route.ts
@@ -1,14 +1,14 @@
 export async function POST(req: Request) {
-    try {
+  try {
       const { restaurantName, category, openingHours, deliveryTime, description } = await req.json();
       
       await db.restaurant.create({
-        data: { restaurantName, category, openingHours, deliveryTime, description }
+          data: { restaurantName, category, openingHours, deliveryTime, description }
       });
-  
+
       return new Response(JSON.stringify({ message: 'Informações do restaurante salvas com sucesso!' }), { status: 200 });
-    } catch (error) {
+  } catch (error) {
+      console.error("Erro ao salvar restaurante:", error); // Log do erro
       return new Response(JSON.stringify({ message: 'Erro ao salvar informações do restaurante' }), { status: 500 });
-    }
   }
-  
+}
